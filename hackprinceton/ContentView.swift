85b003473e9290d6dd9c4907824efd4213d844f8
Anthony Li <me@anli.dev>
2024-03-30 10:14:02 -0400

    Plot commit graph in AR

--- hackprinceton/ContentView.swift
+++ hackprinceton/ContentView.swift
@@ -11,37 +11,11 @@ import RealityKit
 
 struct ContentView : View {
     @State var githubAPI = GitHubAPI(repositoryURL: "https://github.com/torvalds/linux")!
-    @State var graphNodes = [CommitGraphNode]()
     @State var commit: Commit? = nil
     
     var body: some View {
-        ZStack {
-            /* ARViewContainer()
-                .edgesIgnoringSafeArea(.all) */
-            
-            ScrollView {
-                VStack(alignment: .leading) {
-                    ForEach(graphNodes) { node in
-                        let commit = githubAPI.repository.commits[node.sha]!
-                        
-                        Button {
-                            self.commit = commit
-                        } label: {
-                            HStack {
-                                Circle()
-                                    .fill(.red)
-                                    .frame(width: 10, height: 10)
-                                Text(commit.title)
-                            }
-                            .padding(.leading, CGFloat(20 * node.x))
-                            .multilineTextAlignment(.leading)
-                        }
-                    }
-                }
-                .padding()
-                .frame(maxWidth: .infinity)
-            }
-        }
+        ARViewContainer(repository: githubAPI.repository)
+            .edgesIgnoringSafeArea(.all)
         .sheet(item: $commit) { commit in
             CommitDetailView(commit: commit)
             .presentationDetents([.fraction(0.4), .large])
@@ -56,28 +30,23 @@ struct ContentView : View {
                 logger.error("Failed to populate repo! \(error)")
             }
         }
-        .onChange(of: githubAPI.repository) {
-            graphNodes = computeGraph(from: githubAPI.repository)
-        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    var repository: Repository
     
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
 
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
-        model.transform.translation.y = 0.05
-
         // Create horizontal plane anchor for the content
         let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
 
+        // Set up the scene
+        setupScene(repository: repository, rootEntity: context.coordinator)
+        anchor.children.append(context.coordinator)
+        
         // Add the horizontal plane anchor to the scene
         arView.scene.anchors.append(anchor)
 
@@ -85,8 +54,14 @@ struct ARViewContainer: UIViewRepresentable {
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        updateScene(repository: repository, rootEntity: context.coordinator)
+    }
     
+    // returns our root entity lmao
+    func makeCoordinator() -> Entity {
+        return Entity()
+    }
 }
 
 #Preview {
