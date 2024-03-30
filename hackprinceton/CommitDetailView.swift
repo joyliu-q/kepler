66d5d68f799be5866f6b799d1a528aedaadd535a
Joy Liu <joyliu.q@gmail.com>
2024-03-30 19:15:40 -0400

    :tada Add contextual tabs based on modal expansion height and refactoring

--- hackprinceton/CommitDetailView.swift
+++ hackprinceton/CommitDetailView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 import CodeViewer
 #endif
 
+extension PresentationDetent {
+    static let customMedium = PresentationDetent.fraction(0.4)
+}
+
 struct CommitDetailView: View {
     var commit: Commit
     @State var githubAPI: GitHubAPI
     
     @State var diff: [String]?
-
-    
+    // TODO: request gpt
+    @State var gptResult: String?
+    @State private var currentPresentationDetent = PresentationDetent.customMedium
 
     private let dateFormatter: DateFormatter = {
             let formatter = DateFormatter()
@@ -29,42 +34,57 @@ struct CommitDetailView: View {
     var body: some View {
         
         VStack {
-            
             Text(dateFormatter.string(from: commit.date)).monospaced().font(.subheadline).foregroundColor(.primary)
             
-            VStack(alignment: .leading) {
-                TabView {
+            if (currentPresentationDetent == PresentationDetent.large) {
+                VStack {
                     
-                    CommitMetadataView(commit: commit)
-                        .tabItem {
-                            Text("Metadata")
-                        }
-                    
-                    #if os(iOS)
-                    if diff != nil {
-                        CommitDiffView(diff: diff!).tabItem {
-                            Text("View Diff")
-                        }
-                    }
-                    #endif
-                }.task({
-                    do {
-                        if let diffStr = try await githubAPI.getDiff(sha: commit.sha) {
-                            diff = [diffStr]
-                        }
-                        
-                    } catch {
-                        logger.error("Failed to populate repo! \(error)")
-                    }
-                    
-                   
-                }).tabViewStyle(.page)
                 
-            }
-            
+                VStack(alignment: .leading) {
+                    TabView {
+                        
+#if os(iOS)
+                        if diff != nil {
+                            CommitDiffView(diff: diff!, commit: commit).tabItem {
+                                Text("View Diff")
+                            }
+                        }
+#endif
+                        
+                        if gptResult != nil {
+                            CommitGptView(gpt: gptResult!, commit: commit).tabItem {
+                                Text("GPT Analyze")
+                            }
+                        }
+                    }.tabViewStyle(.page)
+                }}} else { 
+                    VStack() {
+                        Spacer()
+                        CommitMetadataView(commit: commit)
+                        Spacer()
+                        Button(action: {
+                            currentPresentationDetent = currentPresentationDetent == PresentationDetent.large ? PresentationDetent.customMedium : PresentationDetent.large
+                        }) {
+                            Text("Expand to View Details")
+                        }
+                    }
+                }
         }
+        .task({
+            do {
+                if let diffStr = try await githubAPI.getDiff(sha: commit.sha) {
+                    diff = [diffStr]
+                }
+                
+            } catch {
+                logger.error("Failed to populate repo! \(error)")
+            }
+        })
         .padding(.horizontal)
-        .padding(.top, 16)
+        .presentationDetents([.customMedium, .large], selection: $currentPresentationDetent)
+        .presentationDragIndicator(.visible)
+        .presentationBackgroundInteraction(.enabled)
+        .presentationBackground(.regularMaterial)
     }
 }
 
@@ -72,6 +92,7 @@ struct CommitDetailView: View {
 struct CommitMetadataView: View {
 //    @State var thing = false
     var commit: Commit
+    var hideDescription = false
     
     var body: some View {
         
@@ -130,7 +151,7 @@ struct CommitMetadataView: View {
                         }.monospaced().font(.subheadline)
                     }
                     
-                    if commit.description != nil {
+                    if commit.description != nil && !hideDescription {
                         
                         Text(commit.description!).lineLimit(3)
                     }
@@ -155,9 +176,12 @@ extension String: Identifiable {
 /// View for a Diff commits are associated with
 struct CommitDiffView: View {
     var diff: [String]
+    var commit: Commit
     
     var body: some View {
         VStack() {
+            CommitMetadataView(commit: commit, hideDescription: true)
+            
             VStack(spacing: 16) {
                 ForEach(diff) { d in
                     CodeViewer(
@@ -175,6 +199,22 @@ struct CommitDiffView: View {
     
 }
 #endif
+
+struct CommitGptView: View {
+    var gpt: String
+    var commit: Commit
+
+    var body: some View {
+        VStack() {
+            CommitMetadataView(commit: commit, hideDescription: true)
+            
+            VStack(spacing: 16) {
+                Text(gpt)
+            }.padding(24)
+        }
+    }
+    
+}
 
 
 #Preview {
