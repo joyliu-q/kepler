e6bb7664a18356407c51da88a3689e3e5227ee8d
Anthony Li <me@anli.dev>
2024-03-30 14:45:25 -0400

    Re-implement coloring of edges and nodes

--- hackprinceton/AR/ARViewModel.swift
+++ hackprinceton/AR/ARViewModel.swift
@@ -9,37 +9,6 @@ import SwiftUI
 import ARKit
 import RealityKit
 
-let commitSphere = MeshResource.generateSphere(radius: 0.01)
-let commitCollision = CollisionComponent(shapes: [.generateSphere(radius: 0.01)])
-
-
-func createCommitEntity(for commit: Commit, color: CommitColor, isActive: Bool = false) -> Entity {
-    let entityName: String = isActive ? color.getActive() : color.getInactive() // Simplify selection logic for example
-            
-        do {
-            // Try to load the named entity from the specified bundle
-            let scene = try Entity.load(named: "Scene", in: .main)
-            let entity = scene.findEntity(named:entityName)!
-
-            // Optionally customize the entity further if needed
-            // Remember to set your CommitComponent to keep commit data associated
-            entity.components.set(commitCollision)
-            entity.components.set(CommitComponent(commit: commit))
-            return entity
-        } catch {
-            // Handle loading error (e.g., entity not found)
-            fatalError("Error loading entity named \(entityName): \(error)")
-        }
-//    let entity = ModelEntity(mesh: commitSphere, materials: [SimpleMaterial(color: color, roughness: 0.5, isMetallic: true)])
-//    entity.components.set(commitCollision)
-//    entity.components.set(CommitComponent(commit: commit))
-//    return entity
-}
-
-func createCommitEntity(for commit: Commit, color: CommitColor) -> Entity {
-    return createCommitEntity(for: commit, color: color, isActive: false)
-}
-
 let edgeBoxSize: Float = 0.002
 let edgeBox = MeshResource.generateBox(size: edgeBoxSize)
 
@@ -47,48 +16,9 @@ func createEdgeEntity(color: UIColor) -> Entity {
     return ModelEntity(mesh: edgeBox, materials: [SimpleMaterial(color: color, roughness: 1.0, isMetallic: true)])
 }
 
-func placeCommits(from repo: Repository, in timelineRoot: Entity) {
-    let xSpacing: Float = 0.03
-    let y: Float = 0.015
-    let zSpacing: Float = 0.03
-    
-    let nodes = computeGraph(from: repo)
-    
-    for node in nodes {
-        guard let commit = repo.commits[node.sha] else { continue }
-        
-        let commitEntity = createCommitEntity(for: commit, color: node.color, isActive: false) // not sure if right
-        commitEntity.position = SIMD3(x: Float(node.x) * xSpacing, y: y, z: -Float(node.z) * zSpacing)
-        timelineRoot.addChild(commitEntity)
-        
-        for parent in node.parents {
-            let newColor = if (node.parents.first?.color == node.color) {
-                node.color.getUIColor()
-            } else {
-                UIColor.white
-            }
-            let edgeEntity = createEdgeEntity(color: newColor)
-            edgeEntity.position = SIMD3(
-                x: (Float(node.x) + Float(parent.x)) / 2 * xSpacing,
-                y: y,
-                z: -(Float(node.z) + Float(parent.z)) / 2 * zSpacing
-            )
-            
-            let xDiff = Float(node.x - parent.x) * xSpacing
-            let zDiff = Float(parent.z - node.z) * zSpacing
-            let length = sqrt(xDiff * xDiff + zDiff * zDiff)
-            
-            edgeEntity.scale = SIMD3(x: 1, y: 1, z: length / edgeBoxSize)
-            
-            let angle = atan(xDiff / zDiff)
-            edgeEntity.orientation *= simd_quatf(angle: angle, axis: SIMD3(x: 0, y: 1, z: 0))
-            timelineRoot.addChild(edgeEntity)
-        }
-    }
-}
-
 @Observable class ARViewModel: NSObject {
     let rootEntity = Entity()
+    var loadedEntity: Entity?
     
     #if os(iOS)
     var arView: ARView?
@@ -99,6 +29,89 @@ func placeCommits(from repo: Repository, in timelineRoot: Entity) {
         session = arView.session
     }
     #endif
+    
+    func createCommitEntity(for commit: Commit, color: UIColor, isActive: Bool = false) -> Entity {
+        let entityName: String = isActive ? "Purple_Active" : "Purple_Inactive"
+        
+        if loadedEntity == nil {
+            loadedEntity = try! Entity.load(named: "Scene", in: .main)
+        }
+        
+        let entity = loadedEntity!.findEntity(named: entityName)!.clone(recursive: true).children[0]
+            
+        // Optionally customize the entity further if needed
+        var material = entity.components[ModelComponent.self]!.materials[0] as! PhysicallyBasedMaterial
+        
+        var hue: CGFloat = 0
+        var saturation: CGFloat = 0
+        var brightness: CGFloat = 0
+        color.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: nil)
+        
+        let brighterColor = UIColor(hue: hue, saturation: saturation, brightness: min(1, brightness + 0.2), alpha: 1)
+        
+        material.baseColor = .init(tint: brighterColor)
+        material.emissiveColor = .init(color: color)
+        
+        entity.components[ModelComponent.self]!.materials = [material]
+        
+        // Remember to set your CommitComponent to keep commit data associated
+        entity.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.02)]))
+        entity.components.set(CommitComponent(commit: commit))
+        return entity
+    }
+    
+    func placeCommits(from repo: Repository, in timelineRoot: Entity) {
+        let xSpacing: Float = 0.06
+        let y: Float = 0.025
+        let zSpacing: Float = 0.06
+        
+        let nodes = computeGraph(from: repo)
+        
+        for node in nodes {
+            guard let commit = repo.commits[node.sha] else { continue }
+            
+            let commitEntity = createCommitEntity(for: commit, color: node.color, isActive: false)
+            commitEntity.position = SIMD3(x: Float(node.x) * xSpacing, y: y, z: -Float(node.z) * zSpacing)
+            timelineRoot.addChild(commitEntity)
+            
+            var nodeRed: CGFloat = 0
+            var nodeGreen: CGFloat = 0
+            var nodeBlue: CGFloat = 0
+            node.color.getRed(&nodeRed, green: &nodeGreen, blue: &nodeBlue, alpha: nil)
+            
+            for parent in node.parents {
+                var parentRed: CGFloat = 0
+                var parentGreen: CGFloat = 0
+                var parentBlue: CGFloat = 0
+                parent.color.getRed(&parentRed, green: &parentGreen, blue: &parentBlue, alpha: nil)
+                
+                let gamma: CGFloat = 2
+                let newColor = UIColor(
+                    red: pow(pow(parentRed, gamma) + pow(nodeRed, gamma), 1 / gamma),
+                    green: pow(pow(parentGreen, gamma) + pow(nodeGreen, gamma), 1 / gamma),
+                    blue: pow(pow(parentBlue, gamma) + pow(nodeBlue, gamma), 1 / gamma),
+                    alpha: 1
+                )
+                
+                let edgeEntity = createEdgeEntity(color: newColor)
+                edgeEntity.position = SIMD3(
+                    x: (Float(node.x) + Float(parent.x)) / 2 * xSpacing,
+                    y: y,
+                    z: -(Float(node.z) + Float(parent.z)) / 2 * zSpacing
+                )
+                
+                let xDiff = Float(node.x - parent.x) * xSpacing
+                let zDiff = Float(parent.z - node.z) * zSpacing
+                let length = sqrt(xDiff * xDiff + zDiff * zDiff)
+                
+                edgeEntity.scale = SIMD3(x: 1, y: 1, z: length / edgeBoxSize)
+                
+                let angle = atan(xDiff / zDiff)
+                edgeEntity.orientation *= simd_quatf(angle: angle, axis: SIMD3(x: 0, y: 1, z: 0))
+                timelineRoot.addChild(edgeEntity)
+            }
+        }
+    }
     
     func setup(repository: Repository) {
         let timelineRoot = Entity()
