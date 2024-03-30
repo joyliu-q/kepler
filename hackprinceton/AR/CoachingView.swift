34a7f7f6371fc9ee66cfd1968e0142c4a312bf54
Anthony Li <me@anli.dev>
2024-03-30 11:51:39 -0400

    Add ARCoachingOverlayView

---
+++ hackprinceton/AR/CoachingView.swift
@@ -0,0 +1,24 @@
+//
+//  CoachingView.swift
+//  hackprinceton
+//
+//  Created by Anthony Li on 3/30/24.
+//
+
+import SwiftUI
+import ARKit
+
+struct CoachingView: UIViewRepresentable {
+    @Environment(ARViewModel.self) var arViewModel
+    
+    func makeUIView(context: Context) -> ARCoachingOverlayView {
+        let view = ARCoachingOverlayView()
+        view.goal = .horizontalPlane
+        view.session = arViewModel.session
+        return view
+    }
+    
+    func updateUIView(_ uiView: ARCoachingOverlayView, context: Context) {
+        uiView.session = arViewModel.session
+    }
+}
